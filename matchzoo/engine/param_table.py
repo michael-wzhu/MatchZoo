a6f7a530cd2fe54af299cf817afe0143257e397f
wqh17101 <597935261@qq.com>
2018-06-29 17:45:39 +0800

    update

--- matchzoo/engine/param_table.py
+++ matchzoo/engine/param_table.py
@@ -38,9 +38,7 @@ class ParamTable(object):
             raise TypeError("Only accepts a Param instance.")
         if param.name in self._params:
             if True:
-                msg = f"aaaParameter named {param.name} already exists.\n" \
-                      f"To re-assign parameter {param.name} value, " \
-                      f"use `params[\"{param.name}\"] = value` instead."
+                msg = f"aaaParameter named {param.name} already exists.\n"
                 raise ValueError(msg)
         self._params[param.name] = param
 
